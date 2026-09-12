71dab31ccdf4d176bdc016065591d6b9707cd14b
soufiane22 <tanger.soufian2015@gmail.com>
2026-09-12 20:36:36 +0100

    Initial commit: Spring Boot + PostgreSQL setup

---
+++ src/main/java/dev/soufiane/factoo/FactooApplication.java
@@ -0,0 +1,13 @@
+package dev.soufiane.factoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/soufiane/factoo/FactooApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.soufiane.factoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
